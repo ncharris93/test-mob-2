ae7c922bf1fa9382c2e8b415025044a483b6b6a2
Noah <ncharris93@gmail.com>
2021-05-04 12:57:50 -0400

    Squashed commit of the following:
    commit 6ae83b755add5ea6b6b4a4c6cfd21391fadd9775
    Author: Noah <ncharris93@gmail.com>
    Date:   Tue May 4 12:57:33 2021 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 2d25183d94264f629c10fc60c90498fa435ff54b
    Author: knolde <knolde@nexient.com>
    Date:   Tue May 4 12:55:18 2021 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    
    commit 074c76b4b27588ec1db419ea6d7e4a1525bedd35
    Author: Noah <ncharris93@gmail.com>
    Date:   Tue May 4 12:46:22 2021 -0400
    
        mob next [ci-skip] [ci skip] [skip ci]
    Co-authored-by: knolde <knolde@nexient.com>

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
 const main = () => {
   console.log('Hey!')
+  const getRandom = () => {
+    return Math.random()
+  }
+  if (getRandom() > 0.5) {
+    console.log('random is less than 0.5')
+  }
+  if (getRandom() === 0.7) {
+    console.log('0.7')
+  }
+
+  const getMoreStuff = () => {
+    return Array(25)
+  }
 }
 
 main()
